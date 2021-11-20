560389d215e84b79bb3d8183d9f5f10929bdfad2
smartit-iot <smartit.iot.in@gmail.com>
2021-11-20 16:29:18 +0530

    Third commit

--- index.js
+++ index.js
@@ -2,12 +2,59 @@
 const express = require("express");
 const app = express();
 var http = require('http');
-var port = 5000;
-app.set('port', port);
-app.get('/',(req,res)=>{
-    res.send('SmartIT Web Server');
-});
+require('dotenv').config();
+
 var server = http.createServer(app);
-server.listen(port,()=>{
-    console.log('server lisining on port '+port)
+
+server.listen(process.env.PORT,()=>{
+    console.log('server is online')
 });
+
+app.get('/',(req,res)=>{
+  res.send('SmartIT Web Server');
+});
+
+//////////////////Web socket/////////////////////////
+
+const io = require('socket.io')(server);
+
+io.on("connect",(socket)=>{
+    console.log('New user connected');
+
+    socket.emit("auth","Check Is Device Is Online");
+
+    socket.on("status", (data)=>{
+      console.log(data);
+    })
+
+    socket.on("JoinRoom",(room)=>{
+      console.log(room);
+      socket.join(room)
+    });
+
+    socket.on("message",({ roomName, deviceName, switchNo, switchStatus })=>{
+      console.log({ roomName, deviceName, switchNo, switchStatus })
+      let data = 0;
+      switch(switchNo){
+        case 1:
+          switchStatus === true ? data = 1 : data = 2;
+          break;
+        case 2:
+          switchStatus === true ? data = 3 : data = 4;
+          break;
+        case 3:
+          switchStatus === true ? data = 5 : data = 6;
+          break;
+        case 4:
+          switchStatus === true ? data = 7 : data = 8;
+          break;
+        default: 
+          data=0;
+      }
+      console.log(data);
+      socket.to(roomName).emit( deviceName, data );
+    });
+
+});
+
+//////////////////Web socket/////////////////////////
