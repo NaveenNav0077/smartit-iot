1fbe0901b011b88b2806d46997e1e58b56f08670
Naveen Kumar K <naveennav0077@gmail.com>
2023-02-26 22:51:41 +0530

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// "start": "NODE_ENV=production nodemon index.js"
 const express = require("express");
 const app = express();
 var http = require('http');
@@ -19,37 +18,17 @@ app.get('/',(req,res)=>{
 const io = require('socket.io')(server);
 
 io.on("connect",(socket)=>{
-
+    console.log("connected")
     socket.emit("AUTH","");
-
     socket.on("JOINROOM",(room)=>{
       if(room !== null && room !== undefined ){
-        console.log(room)
-        if(typeof room == "object" && room?.gId !== null && room?.gId !== undefined){
-          socket.join(room?.gId?.toString());
-        }
-        else if(typeof room == "string"){
-          socket.join(room?.toString());
-        }
+        socket.join(room?.device_id?.toString());
       }
     });
-
-    socket.on("GET_STATUS", (data)=>{
-      if(data?.Room_Name!==undefined && data?.Device_Name!==undefined){
-          socket.to(data?.Room_Name?.toString()).emit( data?.Device_Name+"_STATUS", "" );
-      }
-    })
     
-    socket.on("STATUS", (data)=>{
-      if(data!==undefined){
-        if(data?.Room_Name!==undefined && data?.Device_Name!==undefined){
-            socket.to(data?.Room_Name?.toString()).emit("DEVICE_APP", data);
-        }
-      }
-    })
-
-    socket.on("ONCHANGE",({ roomName, deviceName, switchNo, switchStatus })=>{
-      socket.to(roomName?.toString()).emit( deviceName, { switchNo, switchStatus:!switchStatus } );
+    socket.on("ONCHANGE",(data)=>{
+      console.log(data)
+      socket.to(data.device_id?.toString()).emit( "AAAAAA", { switchNo:data.switchNo, switchStatus:!data.switchStatus } );
     });
 
 });
